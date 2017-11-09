f99fba32f7944798da07070e08d683505778e466
JunhaoGao <carelessgao@gmail.com>
2017-11-09 16:23:54 +0800

    get train data file list

--- getTrainDatalist.py
+++ getTrainDatalist.py
@@ -1,23 +1,28 @@
 import os
+import cv2
 
-trainDataPath = 'D:\\detectProject\\traindata'
-file_objectS = open('D:\\detectProject\\SmallTrainData.txt', 'w')
-file_objectM = open('D:\\detectProject\\MiddleTrainData.txt', 'w')
-file_objectL = open('D:\\detectProject\\LargeTrainData.txt', 'w')
+trainDataPath = 'D:\\detectProject\\data\\sourceData\\TRAINDATA\\man'
+file_objectS = open('D:\\detectProject\\data\\sourceData\\TRAINDATA\\SmallTrainData.txt', 'w')
+file_objectM = open('D:\\detectProject\\data\\sourceData\\TRAINDATA\\MiddleTrainData.txt', 'w')
+file_objectL = open('D:\\detectProject\\data\\sourceData\\TRAINDATA\\LargeTrainData.txt', 'w')
 
 small, middle, large = 0, 0, 0
+
+
 
 for root, dirs, files in os.walk(trainDataPath):
     for trainData in files:
         tdPath = str(trainData)
-        if str(trainData)[0] == 'S':
-            file_objectS.write(tdPath + '\n')
+        image = cv2.imread(trainDataPath + '\\' + tdPath)
+
+        if image.shape[0] == 30:
+            file_objectS.write(trainDataPath + '\\' + tdPath + '\n')
             small += 1
-        elif str(trainData)[0] == 'M':
-            file_objectM.write(tdPath + '\n')
+        elif image.shape[0] == 50:
+            file_objectM.write(trainDataPath + '\\' + tdPath + '\n')
             middle += 1
-        elif str(trainData)[0] == 'L':
-            file_objectL.write(tdPath + '\n')
+        elif image.shape[0] == 100:
+            file_objectL.write(trainDataPath + '\\' + tdPath + '\n')
             large += 1
 
 print 'small: ' + str(small)
